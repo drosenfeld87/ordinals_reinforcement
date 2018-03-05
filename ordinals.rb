c411fc09a951d4fa36f0d44ac76c2537805b16b7
debbie <drosenfeld87@gmail.com>
2018-03-05 15:33:32 -0500

    fixed method so it works now

--- ordinals.rb
+++ ordinals.rb
@@ -1,21 +1,16 @@
 #i know this doesn't work, will ask for help on Monday
 
-
-number = numbers.to_s
-
 def ordinal_indicator(number)
 
-  (1..20).each do |number|
-    if number == "1"
+    if number == 1
       puts "#{number}st"
-    elsif number == "2"
+    elsif number == 2
       puts "#{number}nd"
-    elsif number == "3"
+    elsif number == 3
       puts "#{number}rd"
     else number
       puts "#{number}th"
     end
   end
-end
 
-puts numbers
+ordinal_indicator(1)
